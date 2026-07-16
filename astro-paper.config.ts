ce78f8b333f39f520527b3061b8c39ffd78fbd08
Shane Jones <shane@Shanes-MacBook-Pro.local>
2026-07-16 18:05:56 -0400

    Setting up links

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -29,10 +29,9 @@ export default defineAstroPaperConfig({
     search: "pagefind",
   },
   socials: [
-    { name: "github",   url: "https://github.com/satnaing/astro-paper" },
-    { name: "x",        url: "https://x.com/username" },
-    { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
-    { name: "mail",     url: "mailto:yourmail@gmail.com" },
+    { name: "github",   url: "hhttps://github.com/shanecjones1999" },
+    { name: "linkedin", url: "https://www.linkedin.com/in/shanecjones/" },
+    { name: "mail",     url: "mailto:shanecjones1999@gmail.com" },
   ],
   shareLinks: [
     { name: "whatsapp", url: "https://wa.me/?text=" },
